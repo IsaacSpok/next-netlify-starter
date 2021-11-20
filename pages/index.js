1f1e69ecb1e984314e15606beefdcc6a46662f56
IsaacSpok <89391640+IsaacSpok@users.noreply.github.com>
2021-11-20 18:39:05 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import * as React from "react"
 
 export default function Home() {
-  window.location = "https://tremp.page.link/AqZgL4D8UL97wbrbA";
+  React.useEffect(() => {
+    // window is accessible here.
+    window.location = "https://tremp.page.link/AqZgL4D8UL97wbrbA";
+    console.log("window.innerHeight", window.innerHeight);
+  }, []);
   return (
     <div className="container">
       <Head>
