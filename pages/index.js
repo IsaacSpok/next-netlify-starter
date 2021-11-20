6a946bf355519c1beb8a57408c2ec19656e1abab
IsaacSpok <89391640+IsaacSpok@users.noreply.github.com>
2021-11-20 23:27:57 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Tremp!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          You'll be redirected in a few moments...
         </p>
       </main>
 
